65cb5671ee0c920dccf457e3fee12e5ce90198a3
Paritosh <paritoshverma97@gmail.com>
2017-04-24 22:49:28 +0530

    updating files!

--- client1.c
+++ client1.c
@@ -40,7 +40,7 @@ int main(int argc,char* argv[]){
 	struct sockaddr_in serverAddr,userAddr;
 
 	socketfd = socket(PF_INET,SOCK_STREAM,0);
-	socket_p2p = socket(PT_INET,SOCK_STREAM,0);
+	socket_p2p = socket(PF_INET,SOCK_STREAM,0);
 
 	bzero((char *)&serverAddr,sizeof serverAddr);
 	bzero((char *)&userAddr,sizeof userAddr);
@@ -50,8 +50,8 @@ int main(int argc,char* argv[]){
 
 	if(argc<3) fprintf(stderr,"No port provided.\n");
 
-	serverAddr.sin_port = htons(atoi(argv[2]));
-	serverAddr.sin_addr.s_addr= inet_addr(argv[1]);
+	serverAddr.sin_port = htons(8000);
+	serverAddr.sin_addr.s_addr= inet_addr("127.0.0.1");
 
 	if(connect(socketfd,(struct sockaddr*)&serverAddr,sizeof serverAddr)<0){
 		fprintf(stderr,"CANNOT CONNECT TO CENTRAL CHAT SERVER\n");
@@ -66,14 +66,13 @@ int main(int argc,char* argv[]){
 	send(socketfd,handle,strlen(handle),0);
 	recv(socketfd,in_buff,1024*sizeof(char),0);
 
-	if(strcmp(in_buff,"0")==0) 
+	if(strcmp(in_buff,"0")==0)
 	{
 		recv(socketfd,in_buff,1024*sizeof(char),0);
 		printf("%s\n",in_buff); 
-	} 
+	}
 	else
 	{
-		
 
 		recv(socketfd,user_ip,128*sizeof(char),0);
 		recv(socketfd,user_port,128*sizeof(char),0);
@@ -82,20 +81,18 @@ int main(int argc,char* argv[]){
 		userAddr.sin_port = htons(atoi(user_port)); 	
 		// create_connection
 
-		if(connect(socket_p2p,(struct sockAddr*)&userAddr,sizeof userAddr)<0){
+		if(connect(socket_p2p,(struct sockaddr*)&userAddr,sizeof userAddr)<0){
 			
 			fprintf(stderr,"CANNOT CONNECT TO REQUESTED CHAT CLIENT\n");
 			exit(1);
-		}	
+	}	
 
-		while(1){
 			pthread_t thread_read,thread_write;
 			pthread_create(&thread_read,NULL,readsock,(void*)&socket_p2p);
 			pthread_create(&thread_write,NULL,writesock,(void*)&socket_p2p);
 			pthread_detach(thread_read);
 			pthread_detach(thread_write);
-			break;
-		}
+
 		pthread_exit(0);
 
 	}
--- server1.c
+++ server1.c
@@ -9,6 +9,7 @@
 
 char out_buff[1024];
 char in_buff[1024];
+char handle[128];
 void* readsock(void *socket){
 	int *socketfd;
 	socketfd=(int*)(socket);
@@ -29,27 +30,57 @@ void *writesock(void *socket){
 		memset(out_buff,0,1024*sizeof(char));
 	}
 }
-char users[10][100] = {"cat","dog","bull","sheep"};
-char pass[10][100] = {"meow","bhow","moun","maain"};
+
 int main(int argc,char* argv[]){
+	
+	int socket_cs;
 	int socketfd,newsocket;
-	struct sockaddr_in serverAddr,cliAddr;
+	struct sockaddr_in serverAddr,cliAddr,cServerAddr;
+	char interface[] = "wlp3s0";
+	char cs_addr[128];
+
 	socklen_t cli_size;
 	socketfd = socket(PF_INET,SOCK_STREAM,0);
+	socket_cs = socket(PF_INET,SOCK_STREAM,0);
 	bzero((char *)&serverAddr,sizeof serverAddr);
+	bzero((char *)&cServerAddr,sizeof cServerAddr);
+	cServerAddr.sin_family = AF_INET;
 	serverAddr.sin_family = AF_INET;
 	if(argc<2) fprintf(stderr,"No port provided.\n");
 	serverAddr.sin_port = htons(atoi(argv[1]));
-	serverAddr.sin_addr.s_addr= INADDR_ANY;
+	serverAddr.sin_addr.s_addr = INADDR_ANY;
+	cServerAddr.sin_port = htons(8000);
+	cServerAddr.sin_addr.s_addr = inet_addr("127.0.0.1");	//write server add here
+
+
+	if(connect(socket_cs,(struct sockaddr*)&cServerAddr,sizeof cServerAddr )<0)
+	{
+		fprintf(stderr,"CANNOT CONNECT TO CENTRAL CHAT SERVER");
+	}
+
+	printf("Enter your chat system handle\n");
+	scanf("%s",handle);
+
+	printf("Enter your chat server ip address\n"); 
+	scanf("%s",cs_addr);
+
+	strcpy(out_buff,"add");
+	send(socket_cs,out_buff,strlen(out_buff),0);
+	send(socket_cs,handle,strlen(handle),0);
+	send(socket_cs,cs_addr,strlen(cs_addr),0);
+	send(socket_cs,argv[1],strlen(argv[1]),0);
+
+	close(socket_cs); 
 
 	if(bind(socketfd,(struct sockaddr*)&serverAddr,sizeof serverAddr)<0){
 		fprintf(stderr,"Error binding socket.\n");
 		exit(1);
 	}
-	if(listen(socketfd,5)!=0){
+	if(listen(socketfd,1)!=0){
 		fprintf(stderr, "Error making socket passive.\n");
 		exit(1);
 	}
+
 	while(1){
 		cli_size = sizeof cliAddr;
 		newsocket = accept(socketfd,(struct sockaddr*)&cliAddr,&cli_size);
@@ -66,5 +97,6 @@ int main(int argc,char* argv[]){
 			close(newsocket);
 		}
 	}
+
 	pthread_exit(0);
 }
